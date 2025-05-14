baccfb696728ce09ba7a6c53db8f90da0ba1114c
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-05-14 09:00:07 -0300

    Update Astronomia-sim.js

--- client/src/components/Astronomia/Astronomia-sim.js
+++ client/src/components/Astronomia/Astronomia-sim.js
@@ -5,6 +5,8 @@ import React from 'react';
 import aulaop2 from '../docs/simulados-astronomia/Aulão_P2.pdf';
 import gabarito_sim from '../docs/simulados-astronomia/Gabarito_Simulado_P1_OB.pdf';
 import sim_p1 from '../docs/simulados-astronomia/Simulado_P1_OB.pdf';
+import sim_n3 from '../docs/simulados-astronomia/Simulado_OBA_Astronomia_N3___OB_2025.pdf';
+import sim_n4 from '../docs/simulados-astronomia/Simulado_OBA_Astronomia_N4___OB_2025dois.pdf';
 
 const Astronomiasim = () => {
     return (
@@ -12,19 +14,37 @@ const Astronomiasim = () => {
             <h1 class="fontizzao">SIMULADOS - ASTRONOMIA</h1>
             <hr class="line3"></hr>
             <center>
-            <div className="text-container2">
-                <h4 className="corzaoo">
-                    Para os estudantes que desejam testar seus conhecimentos e se preparar para os desafios reais, esta seção de simulados é perfeita. Aqui você encontrará exercícios que simulam as principais provas e competições, ajudando você a treinar e evoluir. Pronto(a) para encarar o próximo teste? 🚀
-                </h4>
-            </div>
-            <br/>
+                <div className="text-container2">
+                    <h4 className="corzaoo">
+                        Para os estudantes que desejam testar seus conhecimentos e se preparar para os desafios reais, esta seção de simulados é perfeita. Aqui você encontrará exercícios que simulam as principais provas e competições, ajudando você a treinar e evoluir. Pronto(a) para encarar o próximo teste? 🚀
+                    </h4>
+                </div>
+                <br />
             </center>
             <div class="fundo">
                 <center>
                     <div className="text-container3">
+                        <h4 className="fontezao7cor">Simulado OBA (N3)</h4>
+                    </div>
+                    <div class="text-container5">
+                        <a class="active" href={sim_n3}>
+                            <h4 className="cordiff">Questões | Gabarito</h4>
+                        </a>
+                    </div>
+                    <br />
+                    <div className="text-container3">
+                        <h4 className="fontezao7cor">Simulado OBA (N4)</h4>
+                    </div>
+                    <div class="text-container5">
+                        <a class="active" href={sim_n4}>
+                            <h4 className="cordiff">Questões | Gabarito</h4>
+                        </a>
+                    </div>
+                    <br />
+                    <div className="text-container3">
                         <h4 className="fontezao7cor">Seletivas Online 2024/2025</h4>
                     </div>
-                    <br/>
+                    <br />
                     <div className="text-container3">
                         <h4 className="fontezao7cor">Aulão do Simulado (P2)</h4>
                     </div>
@@ -36,7 +56,7 @@ const Astronomiasim = () => {
                             <h4 className="cordiff2">Gabarito</h4>
                         </a>
                     </div>
-                    <br/>
+                    <br />
                     <div className="text-container3">
                         <h4 className="fontezao7cor">Simulado (P1)</h4>
                     </div>
@@ -48,7 +68,7 @@ const Astronomiasim = () => {
                             <h4 className="cordiff2">Gabarito</h4>
                         </a>
                     </div>
-                    <br/>
+                    <br />
                 </center>
             </div>
         </div>
